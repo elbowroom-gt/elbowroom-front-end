e2b3dc78be2de173d9f0577fac3fbeefbe40fa5b
Leonard Ricci <ldricci3@gmail.com>
2020-05-05 13:06:28 -0400

    added list of MTA lines, names and hex colors

---
+++ src/components/MTALines.js
@@ -0,0 +1,90 @@
+const lines = [
+    {
+        name: "A",
+        color: "#0039A6"
+    },
+    {
+        name: "C",
+        color: "#0039A6"
+    },
+    {
+        name: "E",
+        color: "#0039A6"
+    },
+    {
+        name: "B",
+        color: "#FF6319"
+    },
+    {
+        name: "D",
+        color: "#FF6319"
+    },
+    {
+        name: "F",
+        color: "#FF6319"
+    },
+    {
+        name: "M",
+        color: "#FF6319"
+    },
+    {
+        name: "G",
+        color: "#6CBE45"
+    },
+    {
+        name: "J",
+        color: "#996633"
+    },
+    {
+        name: "Z",
+        color: "#996633"
+    },
+    {
+        name: "L",
+        color: "#A7A9AC"
+    },
+    {
+        name: "N",
+        color: "#FCCC0A"
+    },
+    {
+        name: "Q",
+        color: "#FCCC0A"
+    },
+    {
+        name: "R",
+        color: "#FCCC0A"
+    },
+    {
+        name: "S",
+        color: "#808183"
+    },
+    {
+        name: "1",
+        color: "#EE352E"
+    },
+    {
+        name: "2",
+        color: "#EE352E"
+    },
+    {
+        name: "3",
+        color: "#EE352E"
+    },
+    {
+        name: "4",
+        color: "#00933C"
+    },
+    {
+        name: "5",
+        color: "#00933C"
+    },
+    {
+        name: "6",
+        color: "#00933C"
+    },
+    {
+        name: "7",
+        color: "#B933AD"
+    },
+]
