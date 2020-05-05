348d197c6124893b8025c9272bb92c7003f30cea
rmeizoso <meizosoricardo@gmail.com>
2020-05-05 13:00:10 -0400

    Create MARTALines.js
    Just added a constant array with MARTA lines

---
+++ src/components/MARTALines.js
@@ -0,0 +1,20 @@
+const lines = [{
+    name: 'Red',
+    color: '#EC2527'
+},
+ {
+    name: 'Green',
+    color: '#69BD47'
+ },
+ {
+    name: 'Blue',
+    color: '#EC2527'
+},
+ {
+    name: 'Gold',
+    color: '#69BD47'
+ }    
+]
+// Green #69BD47
+// Blue #0093D0
+// Gold #FFA500
