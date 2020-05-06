59ce2a8db396dcf7d5f66cbd04ea3d1af329c490
klee734 <klee734@gatech.edu>
2020-05-06 13:59:43 -0400

    populate direction buttons based on line

--- src/components/DirectionSelection.js
+++ src/components/DirectionSelection.js
@@ -2,22 +2,31 @@ import React, { Component } from "react";
 import { Button } from 'react-bootstrap';
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import "../css/DirectionSelection.css";
-
+import { lines } from "./MARTALines.js";
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  chooseDirection,
-  selectDirection
-} from '../app/appstateSlice';
+import { chooseDirection, selectDirection, selectLine } from '../app/appstateSlice';
 
 const DirectionSelection = function() {
+
   const dispatch = useDispatch();
   const direction = useSelector(selectDirection);
-  
+  const currLine = useSelector(selectLine);
+
+  let button1 = "";
+  let button2 = "";
+
+  lines.forEach(element => {
+    if (element["name"] === currLine) {
+      button1 = element["direction"][0]
+      button2 = element["direction"][1]
+    }
+  })
+
   return (
     <ButtonGroup className="buttonGroup">
-      <Button className = {direction === 1 ? "selectedButton" : "noSelectedButton"} onClick={() => dispatch(chooseDirection(1))}>Uptown</Button>
+      <Button className = {direction === 1 ? "selectedButton" : "noSelectedButton"} onClick={() => dispatch(chooseDirection(1))}>{ button1 }</Button>
       <div className="horizontalDivider"/>
-      <Button className = {direction === -1 ? "selectedButton" : "noSelectedButton"} onClick={() => dispatch(chooseDirection(-1))}>Downtown</Button>
+      <Button className = {direction === -1 ? "selectedButton" : "noSelectedButton"} onClick={() => dispatch(chooseDirection(-1))}>{ button2 }</Button>
     </ButtonGroup>
   );
 }
