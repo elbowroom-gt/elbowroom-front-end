8728cb3edfd098569f9edfb1e6df98be059fabf1
Leonard Ricci <ldricci3@gmail.com>
2020-05-07 02:10:54 -0400

    added fancier dropdown option rendering, shows all lines running through station

--- src/components/StationSearchbar.js
+++ src/components/StationSearchbar.js
@@ -6,6 +6,55 @@ import { chooseStation, selectSystem, selectLine } from "../app/appstateSlice";
 // Imagine you have a list of stations that you'd like to autosuggest.
 import { MARTAStations } from "./MARTAStations";
 import { MTAStations } from "./MTAStations";
+import { MARTALines } from "./MARTALines";
+import { MTALines } from "./MTALines";
+
+
+function renderStationMARTA(props, option, snapshot, className) {
+  let lines = [];
+
+  MARTALines.forEach(element => {
+    if (option.lines.includes(element.name)) {
+      lines.push(<div style={{"background-color": element.color,
+                              "borderRadius": "50%",
+                              "width": "20px",
+                              "height": "20px",
+                              "padding": "0.1em",
+                              "float": "right"}}>
+        </div>);
+    }
+  });
+
+  return <button {...props} className={className} type="button" style={{"width": "100%"}}>
+          {option.name}{lines}
+        </button>
+}
+
+function renderStationMTA(props, option, snapshot, className) {
+  let lines = [];
+
+  MTALines.forEach(element => {
+    if (option.lines.includes(element.name)) {
+      lines.push(<div style={{"background-color": element.color,
+                              "borderRadius": "50%",
+                              "color": "white",
+                              "font-family": "'Helvetica', 'Arial', sans-serif",
+                              "font-size": "90%",
+                              "textAlign": "center",
+                              "verticalAlign": "middle",
+                              "width": "20px",
+                              "height": "20px",
+                              "padding": "0.1em",
+                              "float": "right"}}>
+        {element.name}
+        </div>);
+    }
+  });
+
+  return <button {...props} className={className} type="button" style={{"width": "100%"}}>
+          {option.name}{lines}
+        </button>
+}
 
 const StationSearch = function() {
   const current_line = useSelector(selectLine);
@@ -30,7 +79,8 @@ const StationSearch = function() {
           options={stations.filter((station) => station.lines.includes(current_line))}
           placeholder={"Select a station"}
           search
-          onChange={(value) => dispatch(chooseStation(value))}>
+          onChange={(value) => dispatch(chooseStation(value))}
+          renderOption={system === "MTA" ? renderStationMTA : renderStationMARTA}>
         </SelectSearch>
 }
 
