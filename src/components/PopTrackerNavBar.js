c6b15d01aef5daadf9d76a8a3e9fac7f989a8901
Alexander Proschek <alexander.proschek@gmail.com>
2020-05-08 21:24:10 +0200

    Removed usless imports

--- src/components/PopTrackerNavBar.js
+++ src/components/PopTrackerNavBar.js
@@ -1,26 +1,20 @@
 import React from "react";
 import "../css/PopTrackerNavBar.css";
 import SystemSelector from "./SystemSelector.js";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/Navdropdown";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
 
-
-const PopTrackerNavBar = function() {
+const PopTrackerNavBar = function () {
   return (
     // <Navbar bg="light" expand="lg" className="navBar">
     //   <Navbar.Brand>Elbow Room</Navbar.Brand>
     //   <NavDropdown drop="left" className="dropdown" id="basic-navbar-nav">
-    //     <SystemSelector /> 
-    //   </NavDropdown>     
-		// </Navbar>
+    //     <SystemSelector />
+    //   </NavDropdown>
+    // </Navbar>
     <div className="navBar">
       Elbow Room
       <SystemSelector />
     </div>
   );
-}
+};
 
 export default PopTrackerNavBar;
